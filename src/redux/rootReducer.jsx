50fa691a808d6cde30317a1cf9b69cf24a1bdfd1
tareqhasan382 <tareqhasan382@gmail.com>
2024-04-03 16:06:11 +0600

    fix redux endpoind

--- src/redux/rootReducer.jsx
+++ src/redux/rootReducer.jsx
@@ -1,9 +1,8 @@
 import { baseApi } from "./api/baseApi";
 import authSlice from "./auth/authSlice";
-import jobModelSlice from "./jobs/jobModelSlice";
 export const rootReducer = {
   [baseApi.reducerPath]: baseApi.reducer,
-  jobModel:jobModelSlice,
+ 
   auth:authSlice
   
 };
